ff06c55cf6c90049cae3e64e9ab40097d3446b97
SebasLV12 <sebasr_12@hotmail.com>
2022-05-03 17:22:32 -0500

    parte 20/100000000000000000000000000

--- app/src/main/java/pe/edu/ulima/pm/views/CartaView.kt
+++ app/src/main/java/pe/edu/ulima/pm/views/CartaView.kt
@@ -34,11 +34,11 @@ class CartaView: View {
         //alto de cel
         mHeight = View.MeasureSpec.getSize(heightMeasureSpec).toFloat()
         //el maximo entre los dos
-        mSize = Math.max(mWidth/2, mHeight/2).toFloat()
+        mSize = Math.max(mWidth, mHeight).toFloat()
         //el minimo entre los dos
-        mSizeII= Math.min(mWidth/2, mHeight/2)
+        mSizeII= Math.min(mWidth, mHeight)
 //        setMeasuredDimension(mWidth.toInt(), mHeight.toInt())
-        setMeasuredDimension(mSizeII!!.toInt(), (mSizeII!!*1.5f).toInt())
+        setMeasuredDimension((mSizeII!!*1F).toInt(), (mSizeII!!*1f).toInt())
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -51,31 +51,49 @@ class CartaView: View {
     private fun drawCarta(canvas: Canvas) {
         mPaint!!.color = Color.BLACK
         mPaint!!.style = Paint.Style.STROKE
-        mPaint!!.setStrokeWidth(mSizeII!!/15f)
-        canvas.drawRect(0f, mSize!!, mSizeII!!/1f, 0f, mPaint)
-
-        //texto
-
+        mPaint!!.setStrokeWidth(mSizeII!!/12f)
+        canvas.drawRect(0f, mSizeII!!, mSizeII, 0f, mPaint)
 
     }
 
     private fun drawCaracteres(canvas: Canvas){
         //numeros TOP
+        var simb=simbolo!!
+        val colorPalo: Paint = Paint()
+        var negro=Color.BLACK;
+        var rojo=Color.RED;
+
+        if(simb=="corazon"){
+            simb="♥"
+            colorPalo.color=Color.RED;
+        }else if(simb=="espada"){
+            simb="♠"
+            colorPalo.color=Color.BLACK;
+        }else if(simb=="trebol"){
+            simb="♣"
+            colorPalo.color=Color.BLACK;
+        }else if(simb=="diamante"){
+            simb="♦"
+            colorPalo.color=Color.RED;
+        }
         var textoT=Paint(Paint.ANTI_ALIAS_FLAG)
         textoT.color=Color.BLACK
-        textoT.textSize=mSize/8f
+        textoT.textSize=mSize/5f
 
-        canvas!!.drawText(numero!!,mSize/20f,mSizeII/4.1f,textoT)
+        canvas!!.drawText(numero!!,mSizeII!!/10f,mSize!!/4f,textoT)
         //numero BOT
         var textoB=Paint(Paint.ANTI_ALIAS_FLAG)
         textoB.color=Color.BLACK
-        textoB.textSize=mSize/8f
-        canvas!!.drawText(numero!!,mSizeII/1.2f,mSize!!/1.1f,textoT)
+        textoB.textSize=mSize/5f
+        canvas!!.drawText(numero!!,mSizeII!!/1.3f,mSize!!/1f,textoT)
         //simbolos
         var icono=Paint(Paint.ANTI_ALIAS_FLAG)
-        icono.color=Color.BLACK
-        icono.textSize=mSize/6f
-        canvas!!.drawText(simbolo!!,mSizeII!!/3f,mSize!!/1.9f,icono)
+        icono.textSize=mSize/10f
+        icono.textAlign=Paint.Align.CENTER
+        val xPos = (canvas.width / 2)
+        val yPos = (canvas.height / 2 - (icono.descent() + icono.ascent()) / 2)
+
+        canvas!!.drawText(simb!!,xPos.toFloat(),yPos.toFloat(),icono)
 
     }
 
