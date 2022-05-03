dda40ad26404f25c47aa5a6b34f903e932b92f84
SebasLV12 <sebasr_12@hotmail.com>
2022-05-03 00:51:36 -0500

    parte 10/100000000000000000000000000

--- app/src/main/java/pe/edu/ulima/pm/views/CartaView.kt
+++ app/src/main/java/pe/edu/ulima/pm/views/CartaView.kt
@@ -2,45 +2,78 @@ package pe.edu.ulima.pm.views
 
 import android.content.Context
 import android.graphics.*
+import android.util.AttributeSet
 import android.view.View
 import pe.edu.ulima.pm.R
 
 class CartaView: View {
-    var size : Int? = null
-    var paint : Paint? = null
+    val mPaint: Paint = Paint()
+    var mSize: Float = 0f
+    var mSizeII: Float = 0f
+    var mWidth: Float = 0f
+    var mHeight: Float = 0f
     var numero : String ?= null
+    constructor(context: Context, num: String) : super(context) {
+        //Crea una instancia a partir de XML
+        numero =num
 
-    constructor(context : Context, num : String) : super(context) {
-        numero = num
-        paint = Paint()
     }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        //Crea una instancia a partir de XML
+
+
+    }
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        val width = View.MeasureSpec.getSize(widthMeasureSpec)
-        val height = View.MeasureSpec.getSize(heightMeasureSpec)
-        size = Math.min(width, height)
-        setMeasuredDimension(size!!, (size!!*1f).toInt())
+
+        //ancho del cel
+        mWidth = View.MeasureSpec.getSize(widthMeasureSpec).toFloat()
+        //alto de cel
+        mHeight = View.MeasureSpec.getSize(heightMeasureSpec).toFloat()
+        //el maximo entre los dos
+        mSize = Math.max(mWidth, mHeight).toFloat()
+        //el minimo entre los dos
+        mSizeII= Math.min(mWidth, mHeight).toFloat()
+        setMeasuredDimension(mWidth.toInt(), mHeight.toInt())
     }
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        drawFondo(canvas)
-        drawBorde(canvas)
-        drawCarta(canvas)
+
+        drawCarta(canvas!!)
+        drawCaracteres(canvas!!)
     }
-    fun drawCarta(canvas: Canvas?) {
+
+    private fun drawCarta(canvas: Canvas) {
+        mPaint!!.color = Color.BLACK
+        mPaint!!.style = Paint.Style.STROKE
+        mPaint!!.setStrokeWidth(mSizeII!!/15f)
+        canvas.drawRect(0f, mSize!!, mSizeII!!/1f, 0f, mPaint)
+
+        //texto
+
 
     }
 
-    fun drawFondo(canvas: Canvas?) {
-        var color= Color.argb(255,255,255,255)
-        canvas?.drawColor(color)
+    private fun drawCaracteres(canvas: Canvas){
+        //numeros TOP
+        var textoT=Paint(Paint.ANTI_ALIAS_FLAG)
+        textoT.color=Color.BLACK
+        textoT.textSize=mSize/8f
+
+        canvas!!.drawText("1",mSize/20F,mSizeII/4.1f,textoT)
+        //numero BOT
+        var textoB=Paint(Paint.ANTI_ALIAS_FLAG)
+        textoB.color=Color.BLACK
+        textoB.textSize=mSize/8f
+        canvas!!.drawText("1",mSizeII/1.2f,mSizeII/0.9f,textoT)
+        //simbolos
+        var simbolo=Paint(Paint.ANTI_ALIAS_FLAG)
+        simbolo.color=Color.BLACK
+        simbolo.textSize=mSize/6f
+        canvas!!.drawText("♠",mSizeII!!/3f,mSizeII!!/1.1f,simbolo)
 
     }
 
-    fun drawBorde(canvas: Canvas?) {
-        paint!!.color = Color.BLACK
-        paint!!.style = Paint.Style.STROKE
-        paint!!.setStrokeWidth(size!!/20f)
-        canvas!!.drawRect(Rect(1, size!!, size!!, 0),paint!!)
-    }
 }
