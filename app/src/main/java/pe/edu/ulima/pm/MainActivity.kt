6e4ec4e7dd7ac3a623630ca1114357c89fe34174
JAVHM <55992367+JAVHM@users.noreply.github.com>
2022-05-05 00:58:21 -0500

    cccccccccccccccccccccccccccccccccccc

--- app/src/main/java/pe/edu/ulima/pm/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/MainActivity.kt
@@ -244,7 +244,7 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.N)
     fun Turno(mazo: MutableList<CartaObj>, jugadorTurno: MutableList<JugadorObj>, mesa: MutableList<CartaObj>, idj:Int){
         var nombre=findViewById<TextView>(R.id.JugTxt)
-        nombre.text=jugadorTurno[idj].nombre;
+        nombre.text=jugadorTurno[idj].nombre + "(" + jugadorTurno[idj].cartasMano.size + ")" ;
         println("Turno jugador " + jugadorTurno[idj].nombre)
         var idTemp = idj //idj no es modificable
         var noPosee = true
